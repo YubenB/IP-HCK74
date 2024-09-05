5edcf04a6135e4f9fcc3128386395c9094b527ae
yubenbauty <yubenbauty@gmail.com>
2024-09-05 18:04:05 +0700

    completing all endpoints

--- Client/Ymedia/src/router.jsx
+++ Client/Ymedia/src/router.jsx
@@ -1,34 +1,62 @@
 import { createBrowserRouter, Outlet, redirect } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/RegisterPage";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Home";
+import RightSideBar from "./components/Organism/RightSideBar";
+import LeftSideBar from "./components/Organism/LeftSideBar";
+import Profile from "./pages/Profile";
+import PostDetail from "./pages/PostDetail";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
-  {
-    loader: () => {
-      if (!localStorage.getItem("token")) {
-        return redirect("/login");
-      }
-      return null;
-    },
     element: (
-      <>
-        <p>Ini navbar</p>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
         <Outlet />
-      </>
+      </GoogleOAuthProvider>
+    ),
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+  {
+    element: (
+      <div className="flex min-h-screen bg-gray-100 mx-32">
+        <LeftSideBar />
+        <div
+          className="w-1/2 bg-white p-4 flex-1 h-screen overflow-y-auto"
+          id="check"
+        >
+          <Outlet />
+        </div>
+        <RightSideBar />
+      </div>
     ),
     children: [
       {
         path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/postdetail/:id",
+        element: <PostDetail />,
+      },
+      // loader: () => {
+      //   if (!localStorage.getItem("token")) {
+      //     return redirect("/login");
+      //   }
+      //   return null;
+      // },
+      {
+        path: "/profile/:username",
+        element: <Profile />,
       },
     ],
   },
